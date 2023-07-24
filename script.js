125b0f07e960725e2f8e19051e0c774974515089
Jessica Sheoran <jessicasheoran@Jessicas-MacBook-Air.local>
2023-07-24 22:11:26 +0530

    integration

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const quoteText= document.getElementById('quote');
 const authorText= document.getElementById('author');
 const twitterBtn= document.getElementById('twitter');
 const newQuoteBtn= document.getElementById('new-quote');
+const whatsappbutton= document.getElementById('whatsapp');
+const facebookbtn=document.getElementById('facebook');
 let jsonquote=[];
 //show new quote
 function newquote(){
@@ -27,6 +29,8 @@ function newquote(){
 
 
 }
+//show loading function
+
 
 async function GetQuotes(){
     const url='https://jacintodesign.github.io/quotes-api/data/quotes.json';
@@ -45,8 +49,19 @@ function tweetQuote(){
     const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent}-${authorText.textContent}` ;
     window.open(twitterUrl, '_blank'); 
 }
+function getTowhatsapp(){
+    const whatsappurl= 'https://web.whatsapp.com/#';
+    window.open(whatsappurl,'_blank')
+}
+function getfacebook(){
+    const fburl='https://www.facebook.com/';
+    window.open(fburl,'_blank'); 
+
+}
 //event listeners
 newQuoteBtn.addEventListener('click',newquote);
 twitterBtn.addEventListener('click',tweetQuote);
+whatsappbutton.addEventListener('click',getTowhatsapp); 
+facebookbtn.addEventListener('click',getfacebook);
 // driver code
 GetQuotes();
